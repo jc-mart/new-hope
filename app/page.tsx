d7a4af44804009270cc1e8b5096fcd30947c1f68
Juan C Martinez <git@jcmart.dev>
2025-06-22 11:58:16 -0500

    More landing page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
-import { hostGrotesk, lusitana } from "./ui/fonts";
+import { lusitana } from "./ui/fonts";
 import Footer from "./ui/footer";
 
 export default function Home() {
   // TODO: Fetch the latest recorded broadcast if not live.
-  const fbLive: string = "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcognewhope%2Fvideos%2F1222475879385691%2F&width=1280";
+  const fbLive: string = "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcognewhope%2Fvideos%2F1408242260301336%2F&width=1920";
   const gMaps: string = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2972.5758341756728!2d-87.71748512357419!3d41.83743096795906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e326ad5253eef%3A0x1e93f44dae8733ae!2s3046%20S%20Central%20Park%20Ave%2C%20Chicago%2C%20IL%2060623!5e0!3m2!1sen!2sus!4v1750353913929!5m2!1sen!2sus";
 
   return (
-    <div className={`${hostGrotesk.className}`}>
+    <div>
       <main>
+        
         <div className="flex justify-center mt-1 mb-1">
           <Image src='/cog_black.jpg' width={25} height={25} alt='Church of God Logo' />
         </div>
@@ -29,23 +30,32 @@ export default function Home() {
               <div className="aspect-video">
                 {/* Move this to a server action */}
                 <h2 className="text-l md:text-3xl m-5">Latest Sunday Service Broadcast</h2>
+                <hr className="mb-4 ml-24 mr-24" />
                 <iframe src={fbLive} allowFullScreen className="relative w-full h-full"/>
               </div>
 
-              <hr className="mt-8 mb-8 ml-16 mr-16" />
+              <h2 className="text-l md:text-3xl mb-4 mt-8">Visit Us</h2>
+              <hr className="mb-4 ml-24 mr-24" />
 
-              <h2 className="text-l md:text-3xl mb-5">Visit Us</h2>
-              <div className="grid md:grid-cols-2 gap-2">
-                <div>
+              <div className="grid md:grid-cols-3 gap-2">
+                <div className="md:col-span-2">
                   <h2 className="text-l md:text-xl">Located At</h2>
-                  <h3>3046 S. Central Park Ave., Chicago, IL, 60623</h3>
                   <iframe src={gMaps} className="relative w-full h-80 md:h-75"/>
+                  <h3>3046 S. Central Park Ave., Chicago, IL, 60623</h3>
                 </div>
 
                 <div>
                   <h3 className="text-l md:text-xl">Services</h3>
-                  <p>Sunday service at 10:00 a.m.</p>
-                  <p>Wednesday Bible study at 7:00 p.m.</p>
+                  <div className="text-left">
+                    <p className="underline">Sundays at 10:00 A.M.</p>
+                    <ul className="list-disc ml-4">
+                      <li>Worship & Sermon</li>
+                    </ul>
+                    <p className="underline">Wednesdays at 7:00 P.M.</p>
+                    <ul className="list-disc ml-4">
+                      <li>Bible Study</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
 
--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -5,20 +5,23 @@ export default function Footer() {
 	const cogPage: string = "https://churchofgod.org/";
 
 	return (
-		<footer className="grid-cols-1 md:grid-rows-auto mt-16 mb-8">
-			<h3 className="text-center">Quick Links</h3>
+		<footer className="flex justify-center text-sm text-gray-500">
+			<ul className="mt-4 mb-4">
+				<h3 className="text-base text-center">Quick Links</h3>
+					<li>
+						<a href={cogPage} className="flex flex-row gap-2 mt-1 mb-1">
+							<Image src='/cog_globe.jpg' width={20} height={20} alt='Church of God logo' />
+							World Ministries
+						</a>
+					</li>
 
-			<div className="text-sm">
-				<a href={cogPage} className="flex flex-row gap-1 justify-center">
-					<Image src='/cog_globe.jpg' width={20} height={20} alt='Church of God logo' />
-					Church of God
-				</a>
-
-				<a href={fbPage} className="flex flex-row gap-1 justify-center">
-					<Image src='/fb_icon.svg' width={20} height={20} alt='Facebook logo'/>
-					New Hope
-				</a>
-			</div>
+					<li>
+						<a href={fbPage} className="flex flex-row gap-2 mt-1 mb-1">
+							<Image src='/fb_icon.svg' width={20} height={20} alt='Facebook logo'/>
+							New Hope
+						</a>
+					</li>
+			</ul>
 		</footer>
 	)
 }
