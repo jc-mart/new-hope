744bd3e79eda71d47e67c91c3ca335fed3fbdab2
Juan C Martinez <git@jcmart.dev>
2025-06-23 15:57:13 -0500

    Styled for smaller screens

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         </div>
 
         <div className={`${lusitana.className} flex flex-col text-center text-shadow-lg/100 text-white place-content-center bg-[url(/church.jpg)] w-full h-[200px] bg-cover bg-center bg-no-repeat bg-blend-darken md:h-[400px]`}>
-          <h1 className="text-3xl md:text-6xl">New Hope Church of God</h1>
+          <h1 className="text-4xl md:text-6xl">New Hope Church of God</h1>
           {/* TODO: Code up a verse of the day fetcher */}
           {/* This heading will hold the verse of the day */}
           <h3></h3>
@@ -25,34 +25,34 @@ export default function Home() {
         <div className="flex md:flex-row flex-col-reverse justify-center text-center mt-4 mb-4">
           <div className="basis-1/5 hidden md:block"></div>
           
-          <div className="basis-full md:max-w-[750px] pl-1 pr-1 md:pl-4 md:pr-4">
+          <div className="basis-full md:max-w-[750px] md:pl-4 md:pr-4">
             <div className="flex flex-col">
               <div className="aspect-video">
                 {/* Move this to a server action */}
-                <h2 className="text-l md:text-3xl m-5">Latest Sunday Service Broadcast</h2>
+                <h2 className="text-xl md:text-3xl m-5">Latest Sunday Service Broadcast</h2>
                 <hr className="mb-4 ml-24 mr-24" />
                 <iframe src={fbLive} allowFullScreen className="relative w-full h-full"/>
               </div>
 
-              <h2 className="text-l md:text-3xl mb-4 mt-8">Visit Us</h2>
+              <h2 className="text-xl md:text-3xl mb-4 mt-8">Visit Us</h2>
               <hr className="mb-4 ml-24 mr-24" />
 
               <div className="grid md:grid-cols-3 gap-2">
                 <div className="md:col-span-2">
                   <h2 className="text-l md:text-xl">Located At</h2>
-                  <iframe src={gMaps} className="relative w-full h-80 md:h-75"/>
+                  <iframe src={gMaps} className="relative w-full h-80 md:h-75 md:pr-4 md:pl-4 pr-8 pl-8"/>
                   <h3>3046 S. Central Park Ave., Chicago, IL, 60623</h3>
                 </div>
 
                 <div>
                   <h3 className="text-l md:text-xl">Services</h3>
-                  <div className="text-left">
+                  <div className="text-center md:text-left">
                     <p className="underline">Sundays at 10:00 A.M.</p>
-                    <ul className="list-disc ml-4">
+                    <ul className="list-disc ml-4 list-inside">
                       <li>Worship & Sermon</li>
                     </ul>
                     <p className="underline">Wednesdays at 7:00 P.M.</p>
-                    <ul className="list-disc ml-4">
+                    <ul className="list-disc ml-4 list-inside">
                       <li>Bible Study</li>
                     </ul>
                   </div>
