4bcc13a5a4353dcde2b124de20e16b8530da8987
Juan C Martinez <git@jcmart.dev>
2025-06-23 18:48:20 -0500

    Working on getting latest FB livestream

---
+++ app/lib/livestream.tsx
@@ -0,0 +1,11 @@
+'use server';
+/**
+ * Video has a unique ID that can be put into a crafted URL to get the
+ * latest broadcast.
+ */
+// TODO: Poll for latest broadcast one a week
+export async function fetchBroadcast(): Promise<string> {
+    var latestBroadcast = '';
+    
+    return latestBroadcast;
+}
